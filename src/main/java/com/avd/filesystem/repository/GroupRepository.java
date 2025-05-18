3e36549f8a2c3d4e5199846cbdf3c5ee90dcca29
niramaytanna74 <niramaytanna74@gmail.com>
2025-05-19 00:27:30 +0530

    fix issue for MVP

--- src/main/java/com/avd/filesystem/repository/GroupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.avd.filesystem.repository;
-
-import com.avd.filesystem.model.entity.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-    boolean existsByName(String name);
-}
